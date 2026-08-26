1e8da8f66c614ef5f2aba854b5e40caf7daee709
Jolaoluwa Oladipupo Amodu <jola47amodu@gmail.com>
2026-08-25 21:34:47 -0500

    LC-longest-palindromic-substring [Runtime: N/A; Memory: N/A]

---
+++ manacher/longest-palindromic-substring.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        result = ''
+        
+        for i in range(len(s)):
+            # odd
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > len(result):
+                    result = s[l:r + 1]
+                l -= 1
+                r += 1
+
+            # even
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > len(result):
+                    result = s[l:r + 1]
+                l -= 1
+                r += 1
+        return result
+
+        
