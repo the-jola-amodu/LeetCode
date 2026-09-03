514b1a74d787db614196f938bc4eb2da0057747d
Jolaoluwa Oladipupo Amodu <jola47amodu@gmail.com>
2026-09-03 16:56:13 -0500

    LC-wildcard-matching [Runtime: N/A; Memory: N/A]

--- greedy/wildcard-matching.py
+++ greedy/wildcard-matching.py
@@ -9,9 +9,11 @@ class Solution:
 
             if (i < len(s)) and (s[i] == p[j] or p[j] == '?'):
                 return recurse(i + 1, j + 1)
+
             if (i < len(s)) and p[j] == '*':
-                return recurse(i + 1, j)
-            if (i >= len(s)) and p[j] == '*':
+                return recurse(i + 1, j) or recurse(i + 1, j + 1)
+
+            if (i >= len(s)) and p[j] == '*' and j == len(p) - 1:
                 return True
             return False
 
